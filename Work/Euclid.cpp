1edf3d37b72eb68fad1fd434e3669d9508163baa
mingzailao <mingzailao@163.com>
2016-09-14 00:10:55 +0800

    no message

---
+++ Work/Euclid.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+#include "Euclid.h"
+using namespace std;
+int main()
+{
+    int m,n;
+    cout<<"input 2 number"<<endl;
+    cin>>m>>n;
+    cout<<"run the function"<<endl;
+    cout<<Euclid(m,n)<<endl;
+    return 0;
+}
